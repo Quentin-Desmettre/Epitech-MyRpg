528e2d983d24d7c377326d573f18053933e7bd35
Paulin Leclercq <paulin.leclercq@epitech.eu>
2022-05-05 18:20:21 +0200

    [+] displaying the level in the inventory

--- include/rpg.h
+++ include/rpg.h
@@ -431,6 +431,7 @@ void recreate_enemy(game_t *game, ray_c *light);
 sfVector2u get_graphic_pos(sfVector2f pos, game_t *game, ray_c *r);
 void put_wall_around(path_t *p, sfVector2u pos);
 int is_on_water(npc_t *enemy, list_t *items, sfSprite *item);
+void draw_level(game_t *game, sfVector2f size);
 
 static inline sfVector2u get_graphic_size(level_t *l, ray_c *data)
 {
